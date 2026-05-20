d8e264bbd0aa69d71fba5ec5cc33cde1b53c2e88
Ben Le <ben.le@contentful.com>
2026-05-20 00:39:04 -0600

    fix(app): import locations constants from @contentful/app-sdk (sdk.locations undefined)

--- app/src/locations/router.tsx
+++ app/src/locations/router.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { locations } from "@contentful/app-sdk";
 import { AppConfig } from "./app-config";
 import { PageConsole } from "./page-console";
 import { PageFrozen } from "./page-frozen";
 
 export function Router({ sdk }: { sdk: any }) {
   const loc = sdk.location;
-  if (loc.is(sdk.locations.LOCATION_APP_CONFIG)) return <AppConfig sdk={sdk} />;
-  if (loc.is(sdk.locations.LOCATION_PAGE)) {
+  if (loc.is(locations.LOCATION_APP_CONFIG)) return <AppConfig sdk={sdk} />;
+  if (loc.is(locations.LOCATION_PAGE)) {
     const isConsole = sdk.ids.space === sdk.parameters.installation?.consoleSpaceId;
     return isConsole ? <PageConsole sdk={sdk} /> : <PageFrozen sdk={sdk} />;
   }
